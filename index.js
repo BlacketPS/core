f3a5503d5d2d85b293f4ba2ee785ba86be95a974
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2025-10-11 22:59:16 +0100

    closer to working docker image

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require('@prisma/client')
+const prismaPkg = require('@prisma/client');
+const out = { ...prismaPkg };
+// Lift Prisma namespace (enums/constants) onto top-level named exports for compatibility
+if (prismaPkg && prismaPkg.Prisma && typeof prismaPkg.Prisma === 'object') {
+	Object.assign(out, prismaPkg.Prisma);
+}
+module.exports = out;
